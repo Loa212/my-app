c869a3dab1e0a991710aa88de6301b6bba158ae0
Loa212 <anobileloris@gmail.com>
2023-08-10 17:52:40 +0200

    add shadcn

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,30 @@
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Dashboard</h1>
+      <Button>Click me</Button>
+      <Sheet>
+        <SheetTrigger>Open</SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you sure absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
     </main>
   );
 }
